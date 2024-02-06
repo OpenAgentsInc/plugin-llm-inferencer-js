8c9cf20e7ad1c71711281f60089860496aa51f02
Christopher David <chris@arcadelabs.co>
2024-02-06 13:24:28 -0600

    and type for host function

--- plugin.d.ts
+++ plugin.d.ts
@@ -2,3 +2,9 @@ declare module 'main' {
   // Extism exports take no params and return an I32
   export function callOpenAI(): I32;
 }
+
+declare module 'extism:host' {
+  interface user {
+    httpFetch(ptr: I64): I64;
+  }
+}
